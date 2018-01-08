9a0d88eccf651cef6386f9ca42ed602a93f8386b
Eugene Bogdevich <bogdevich96@gmail.com>
2018-01-08 14:43:18 +0300

    Resource server configuration

---
+++ auth/src/main/java/com/bogdevich/auth/config/ResourceServerConfiguration.java
@@ -0,0 +1,54 @@
+package com.bogdevich.auth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
+/**
+ * Resource server configuration class
+ *
+ * {@link EnableResourceServer} enables a resource server.
+ * By default this annotation creates a security filter which authenticates requests via an incoming OAuth2 token.
+ * The filter is an instance of WebSecurityConfigurerAdapter which has an hard-coded order of 3
+ * (Due to some limitations of Spring Framework).
+ * You need to tell Spring Boot to set OAuth2 request filter order to 3
+ * to align with the hardcoded value. You do that
+ * by adding security.oauth2.resource.filter-order = 3 in the application.properties file.
+ *
+ * @author Eugene Bogdevich
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter{
+
+    private ResourceServerTokenServices tokenServices;
+
+    @Value("${security.jwt.resource-ids}")
+    private String resourceIds;
+
+    @Autowired
+    public ResourceServerConfiguration(ResourceServerTokenServices tokenServices) {
+        super();
+        this.tokenServices = tokenServices;
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        super.configure(resources);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .requestMatchers()
+                .and()
+                .authorizeRequests()
+                    .antMatchers("/actuator/**", "/api-docs/**").permitAll()
+                    .antMatchers("/api/**").authenticated();
+    }
+}
