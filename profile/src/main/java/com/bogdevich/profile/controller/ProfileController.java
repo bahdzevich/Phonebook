bcf1b89c2e15328488b8ace78fa07d0fc536b456
Eugene Bogdevich <bogdevich96@gmail.com>
2018-01-10 18:09:39 +0300

    Sructure

--- profile/src/main/java/com/bogdevich/profile/controller/ProfileController.java
+++ profile/src/main/java/com/bogdevich/profile/controller/ProfileController.java
@@ -1,10 +1,45 @@
 package com.bogdevich.profile.controller;
 
+import com.bogdevich.profile.entity.dto.ProfileDTO;
+import com.bogdevich.profile.service.IProfileService;
+import com.bogdevich.profile.service.IRoleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+/**
+ * Rest controller for profile endpoint.
+ *
+ * @author Eugene Bogdevich
+ */
 @RestController
 @RequestMapping("/phonebook/api/profiles")
 public class ProfileController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final IProfileService profileService;
+    private final IRoleService roleService;
+
+    @Autowired
+    public ProfileController(IProfileService profileService, IRoleService roleService) {
+        this.profileService = profileService;
+        this.roleService = roleService;
+    }
+
+    public ResponseEntity<ProfileDTO> getProfileById(Long id);
+
+    public ResponseEntity<List<ProfileDTO>> getProfiles();
+
+    public ResponseEntity<ProfileDTO> createProfile(ProfileDTO profileDTO);
+
+    public ResponseEntity<ProfileDTO> updateProfile(ProfileDTO profileDTO);
+
+    public ResponseEntity<ProfileDTO> deleteProfile(ProfileDTO profileDTO);
+
+    public ResponseEntity<List<ProfileDTO>> findProfileByQuery();
 }
---
+++ profile/src/main/java/com/bogdevich/profile/service/impl/ProjectService.java
@@ -0,0 +1,8 @@
+package com.bogdevich.profile.service.impl;
+
+import com.bogdevich.profile.service.IProjectService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService implements IProjectService{
+}
