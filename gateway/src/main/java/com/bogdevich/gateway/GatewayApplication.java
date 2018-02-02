270ef3bf615ea4d2757399c6c8b6ea218b1051a2
Eugene Bogdevich <bogdevich96@gmail.com>
2018-02-02 06:17:44 +0300

    Zuul filter, Feign client

--- gateway/src/main/java/com/bogdevich/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/bogdevich/gateway/GatewayApplication.java
@@ -1,31 +1,33 @@
 package com.bogdevich.gateway;
 
+import com.bogdevich.gateway.filter.AddPrincipalHeaderFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
-@EnableOAuth2Sso
-public class GatewayApplication extends WebSecurityConfigurerAdapter {
+@EnableResourceServer
+@EnableFeignClients
+public class GatewayApplication {//extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-				.requestMatchers()
-				.and()
-				.authorizeRequests()
-				.antMatchers("/phonebook/api/profiles/csv/**").hasRole("ADMIN")
-				.antMatchers("/phonebook/api/profiles/**").authenticated()
-				.antMatchers("/phonebook/api/news/**").authenticated();
-	}
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http
+//				.requestMatchers()
+//				.and()
+//				.authorizeRequests()
+//				.antMatchers("/phonebook/api/profiles/csv/**").hasRole("ADMIN")
+//				.antMatchers("/phonebook/api/profiles/**").authenticated()
+//				.antMatchers("/phonebook/api/news/**").authenticated();
+//	}
 }
