3d9cc3e6813132cc4a8a6436c427fbcfc238fe27
Eugene Bogdevich <bogdevich96@gmail.com>
2018-02-13 19:57:48 +0300

    Resolving of container naming issue

--- gateway/src/main/java/com/bogdevich/gateway/client/AuthServiceClient.java
+++ gateway/src/main/java/com/bogdevich/gateway/client/AuthServiceClient.java
@@ -1,10 +1,6 @@
 package com.bogdevich.gateway.client;
 
-import feign.HeaderMap;
-import feign.RequestLine;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +8,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Map;
 
-@FeignClient(name = "auth-service")
+@FeignClient(name = "auth")
 public interface AuthServiceClient {
 
     @RequestMapping(
             method = RequestMethod.GET,
-            value = "/phonebook/api/uaa/users/current/info"
+            value = "/phonebook/uaa/api/users/current/info"
     )
     ResponseEntity<Map<String, String>> getPrincipalInfo(@RequestHeader ("Authorization") String token);
 }
